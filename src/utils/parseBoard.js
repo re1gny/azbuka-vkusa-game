bd34ed3d6f3e447e4207fde47710451db836f7ce
dmmanshin <ya.manshin@mail.ru>
2023-12-17 13:29:52 +0300

    fix parse

--- src/utils/parseBoard.js
+++ src/utils/parseBoard.js
@@ -10,11 +10,9 @@ export function parseBoard(board) {
                 return
             }
 
-            const prevVerticalChar = board.chars[y - 1]?.[x]
-            const prevHorizontalChar = board.chars[y]?.[x - 1]
-
-            if (prevVerticalChar) {
-                const prevEntry = entries.find(({positions}) => getFirst(positions)[0] === x
+            if (board.chars[y - 1]?.[x]) {
+                const prevEntry = entries.find(({positions}) =>
+                    getFirst(positions)[0] === x
                     && getLast(positions)[0] === x
                     && getLast(positions)[1] === y - 1
                 )
@@ -23,12 +21,13 @@ export function parseBoard(board) {
                     prevEntry.word += char
                     prevEntry.positions.push([x, y])
                 } else {
-                    entries.push({word: prevVerticalChar + char, positions: [[x, y - 1], [x, y]]})
+                    entries.push({word: board.chars[y - 1][x] + char, positions: [[x, y - 1], [x, y]]})
                 }
             }
 
-            if (prevHorizontalChar) {
-                const prevEntry = entries.find(({positions}) => getFirst(positions)[1] === y
+            if (board.chars[y]?.[x - 1]) {
+                const prevEntry = entries.find(({positions}) =>
+                    getFirst(positions)[1] === y
                     && getLast(positions)[0] === x - 1
                     && getLast(positions)[1] === y
                 )
@@ -37,11 +36,11 @@ export function parseBoard(board) {
                     prevEntry.word += char
                     prevEntry.positions.push([x, y])
                 } else {
-                    entries.push({word: prevHorizontalChar + char, positions: [[x - 1, y], [x, y]]})
+                    entries.push({word: board.chars[y][x - 1] + char, positions: [[x - 1, y], [x, y]]})
                 }
             }
 
-            if (!prevVerticalChar && !prevHorizontalChar) {
+            if (!board.chars[y - 1]?.[x] && !board.chars[y]?.[x - 1]) {
                 entries.push({word: char, positions: [[x, y]]})
             }
         })
